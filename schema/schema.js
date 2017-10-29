e4200242f42499251186431e8ae87ddd4a9ae872
Adam Winick <sinelanguage@gmail.com>
2017-10-28 20:22:23 -0400

    fields objects before converting to a fn

--- schema/schema.js
+++ schema/schema.js
@@ -5,17 +5,44 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema, // takes in RootQuery and returns a schema
+  GraphQLSchema, // takes in RootQuery and returns a schema,
+  GraphQLList
 } = graphql
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id:           { type: GraphQLString },
+    name:         { type: GraphQLString },
+    description:  { type: GraphQLString },
+    users:        {
+      type: new GraphQLList(UserType), // needed to wrap UserType in a GraphQLList constructor to tell CompanyType that users is a list
+      resolve(parentValue, args){
+        axios.get(`http://localhost:3000/companies/${parentValue.id}`)
+          .then(res => res.data)
+      }
+    }
+  }
+})
 
 // used to construct a graphQL object type, not the object itself
 const UserType = new GraphQLObjectType({
   name: 'User', // required property: String Cap
   fields: {
-    id: { type: GraphQLString}, // each field gets a 'type' Object
-    firstName: { type: GraphQLString},
-    age: { type: GraphQLInt},
-  } // required property: Object
+    id:         { type: GraphQLString }, // each field gets a 'type' Object
+    firstName:  { type: GraphQLString },
+    age:        { type: GraphQLInt },
+    company:    { // its important that this key is a different name from the data models key "companyId", this tells graphQL to run the resolve fn
+      type: CompanyType,
+      resolve(parentValue, args){
+        console.log(parentValue, args)
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data)
+      }
+    } // GraphQL doesn't care if its built in or custom
+    // We have to teach GraphQL how to associate a user with a company
+  } // field is a required property: type Object
+  // in order for circular dependencies on a type declared after this type, to work, we need to wrap the fields object in a fn
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -30,6 +57,15 @@ const RootQuery = new GraphQLObjectType({
         return axios.get(`http://localhost:3000/users/${args.id}`) // resolve can handle a promise which is what axios returns
                 .then(resp => resp.data) // this is axios specific as the response is always an object on the data property of axios which is what we don't want, we just want raw json data
 
+      }
+    },
+
+    company: {
+      type: CompanyType,
+      args: {id: {type: GraphQLString}},
+      resolve( parentValue, args ) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+                .then(resp => resp.data)
       }
     }
   }
